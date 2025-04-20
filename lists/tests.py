197932d831f7fed7369c30a9225b0c022601894a
kyreniabupt <Kyreniaws@163.com>
2025-04-20 17:40:03 +0800

    message 8:Before refactoring the functional_tests

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         reponse = self.client.get('/')
         self.assertTemplateUsed(reponse, 'home.html')
+    
+    def test_home_page_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
